f44f0aafadb5a52bde08feb38067778c3468a5fe
Sahil Hingorani <sahilhingorani25@gmail.com>
2017-07-26 19:05:32 -0700

    Second commit

--- js/crypto.controller.js
+++ js/crypto.controller.js
@@ -5,10 +5,12 @@ angular
 function CryptoController(CryptoService) {
   var ctrl = this;
   var list = [];
+  ctrl.savedCoins = [];
+  ctrl.searchedCoin = '';
 
-  ctrl.onSearch = onSearch;
+  // ctrl.searchedCoin = '';
 
-  function getCrypto() {
+  function getCrypto() {                      //pulls data from API
     CryptoService
       .retrieve()
       .then(function(response) {
@@ -16,12 +18,40 @@ function CryptoController(CryptoService) {
         });
   }
 
-  function onSearch() {
-    console.log("Changed!", ctrl.searchedCoin);
-  }
+  ctrl.showSearch = function() {                //gets rid of search list once input is cleared
+    return ctrl.searchedCoin === '';
+  };
 
+  ctrl.addToList = function(coins) {            //add coins to the savedCoins list
+    var temp = 0;
+    var results = {
+      name: coins.name,
+      price_usd: coins.price_usd,
+      symbol: coins.symbol
+    };
+    if(ctrl.savedCoins.length > 0) {
+      for(var i = 0; i < ctrl.savedCoins.length; i++) {
+        if(coins.name === ctrl.savedCoins[i].name) {
+          alert("You have already added this coin!");
+          temp = 1;
+        }
+      }
+    }
+    if(temp === 0) {
+      ctrl.savedCoins.push(results);
+    }
+    console.log(ctrl.savedCoins);
 
+  };
 
+  ctrl.removeCoin = function(coin, index) {         //removes coins from the saved list
+      ctrl.savedCoins.splice(index, 1);
+  };
 
-  getCrypto();
+  ctrl.setPrice = function() {                    //adds the
+
+  };
+
+  
+    getCrypto();
 }
